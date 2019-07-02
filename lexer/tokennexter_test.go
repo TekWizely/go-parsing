3aba8b64a29e80008f37ed6f8da2770955641b7b
TekWize.ly <TekWize.ly@gmail.com>
2019-07-01 22:17:43 -0700

    lexer: Fixed go-critic ifElseChain nag

--- lexer/tokennexter_test.go
+++ lexer/tokennexter_test.go
@@ -11,15 +11,16 @@ import (
 //
 func expectNexterEOF(t *testing.T, nexter token.Nexter) {
 	tok, err := nexter.Next()
-	if err == nil {
-		if tok == nil {
-			t.Errorf("Nexter.Next() expecting (nil, EOF), received (nil, nil)")
-		} else {
-			t.Errorf("Nexter.Next() expecting (nil, EOF), received ({%d, '%s'}, nil)", tok.Type(), tok.Value())
-		}
-	} else if tok != nil {
+	// Used switch per go-critic ifElseChain nag
+	//
+	switch {
+	case err == nil && tok == nil:
+		t.Errorf("Nexter.Next() expecting (nil, EOF), received (nil, nil)")
+	case err == nil && tok != nil:
+		t.Errorf("Nexter.Next() expecting (nil, EOF), received ({%d, '%s'}, nil)", tok.Type(), tok.Value())
+	case err != nil && tok != nil:
 		t.Errorf("Nexter.Next() expecting (nil, EOF), received ({%d, '%s'}, '%s')'", tok.Type(), tok.Value(), err.Error())
-	} else if err != io.EOF {
+	case err != nil && tok == nil && err != io.EOF:
 		t.Errorf("Nexter.Next() expecting (nil, EOF), received (nil, '%s')", err.Error())
 	}
 }
@@ -28,15 +29,16 @@ func expectNexterEOF(t *testing.T, nexter token.Nexter) {
 //
 func expectNexterNext(t *testing.T, nexter token.Nexter, typ token.Type, value string) {
 	tok, err := nexter.Next()
-	if tok == nil {
-		if err == nil {
-			t.Errorf("Nexter.Next() expecting ({%d, '%s'}, nil), received (nil, nil)'", typ, value)
-		} else {
-			t.Errorf("Nexter.Next() expecting ({%d, '%s'}, nil), received (nil, '%s')'", typ, value, err.Error())
-		}
-	} else if err != nil {
+	// Used switch per go-critic ifElseChain nag
+	//
+	switch {
+	case tok == nil && err == nil:
+		t.Errorf("Nexter.Next() expecting ({%d, '%s'}, nil), received (nil, nil)'", typ, value)
+	case tok == nil && err != nil:
+		t.Errorf("Nexter.Next() expecting ({%d, '%s'}, nil), received (nil, '%s')'", typ, value, err.Error())
+	case tok != nil && err != nil:
 		t.Errorf("Nexter.Next() expecting ({%d, '%s'}, nil), received ({%d, '%s'}, '%s')'", typ, value, tok.Type(), tok.Value(), err.Error())
-	} else if tok.Type() != typ || tok.Value() != value {
+	case tok != nil && err == nil && (tok.Type() != typ || tok.Value() != value):
 		t.Errorf("Nexter.Next() expecting ({%d, '%s'}, nil), received ({%d, '%s'}, nil)'", typ, value, tok.Type(), tok.Value())
 	}
 }
@@ -45,15 +47,16 @@ func expectNexterNext(t *testing.T, nexter token.Nexter, typ token.Type, value s
 //
 func expectNexterError(t *testing.T, nexter token.Nexter, errMsg string) {
 	tok, err := nexter.Next()
-	if err == nil {
-		if tok == nil {
-			t.Errorf("Nexter.Next() expecting (nil, '%s'), received (nil, nil)", errMsg)
-		} else {
-			t.Errorf("Nexter.Next() expecting (nil, '%s'), received ({%d, '%s'}, nil)", errMsg, tok.Type(), tok.Value())
-		}
-	} else if tok != nil {
+	// Used switch per go-critic ifElseChain nag
+	//
+	switch {
+	case err == nil && tok == nil:
+		t.Errorf("Nexter.Next() expecting (nil, '%s'), received (nil, nil)", errMsg)
+	case err == nil && tok != nil:
+		t.Errorf("Nexter.Next() expecting (nil, '%s'), received ({%d, '%s'}, nil)", errMsg, tok.Type(), tok.Value())
+	case err != nil && tok != nil:
 		t.Errorf("Nexter.Next() expecting (nil, '%s'), received ({%d, '%s'}, '%s')", errMsg, tok.Type(), tok.Value(), err.Error())
-	} else if err.Error() != errMsg {
+	case err != nil && tok == nil && err.Error() != errMsg:
 		t.Errorf("Nexter.Next() expecting (nil, '%s'), received (nil, '%s')", errMsg, err.Error())
 	}
 }
